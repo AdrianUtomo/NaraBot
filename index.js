f7ab5b3edf6866485fb3d4b4ef3aa01a184c8155
Adrian <adrianhakimutomo@gmail.com>
2022-06-30 22:50:31 +0700

    remove useless comments

--- index.js
+++ index.js
@@ -44,59 +44,6 @@ client.on('messageCreate', async message => {
 		console.error(error)
 		message.channel.send('There is something wrong :(')
 	}
-
-	// if (command === "hello") {
-	// 	message.channel.send('hi')
-	// }
-
-	// else if (command === "apex") {
-	// 	if (!args.length) {
-	// 		return message.channel.send(`You have not input any arguments, ${message.author} :(`)
-	// 	}
-	// 	else if (args[0] === 'map') {
-	// 		try {
-	// 			const mapRotationEmbed = {
-	// 				color: 0x0099ff,
-	// 				title: 'Lorem Ipsum',
-	// 				fields: [
-	// 					{
-	// 						name: 'Current',
-	// 						value: 'lorem ipsum',
-	// 					},
-	// 					{
-	// 						name: 'Next',
-	// 						value: 'lorem ipsum',
-	// 					},
-	// 				],
-	// 				timestamp: new Date(),
-	// 			};
-	// 			const getMap = async() => {
-	// 				const mapRotation = await axios.get(`https://api.mozambiquehe.re/maprotation?auth=${apexAPIKey}&version=2`)
-	// 				const data = mapRotation.data
-	// 				if (args[1] === 'arena') {
-	// 					mapRotationEmbed.title = "Arena Map Rotation"
-	// 					mapRotationEmbed.fields[0].value = `${data.arenas.current.map}\nTime left : ${data.arenas.current.remainingTimer}`
-	// 					mapRotationEmbed.fields[1].value = `${data.arenas.next.map}`
-	// 				}
-	// 				else if (args[1] === 'ranked') {
-	// 					mapRotationEmbed.title = "Ranked Battle Royale Map Rotation"
-	// 					mapRotationEmbed.fields[0].value = `${data.ranked.current.map}\nTime left : ${data.ranked.current.remainingTimer}`
-	// 					mapRotationEmbed.fields[1].value = `${data.ranked.next.map}`
-	// 				}
-	// 				else {
-	// 					mapRotationEmbed.title = "Battle Royale Map Rotation"
-	// 					mapRotationEmbed.fields[0].value = `${data.battle_royale.current.map}\nTime left : ${data.battle_royale.current.remainingTimer}`
-	// 					mapRotationEmbed.fields[1].value = `${data.battle_royale.next.map}`
-	// 				}
-	// 				await message.channel.send({ embeds: [mapRotationEmbed] })
-	// 			}
-	// 			getMap()
-	// 		}
-	// 		catch(e) {
-	// 			await message.channel.send("Error, something is wrong :(")
-	// 		}
-	// 	}
-	// }
 })
 
 client.login(process.env.TOKEN);
