c30383870b6fcc801c91edc2062c6774daed0fd0
Adrian <adrianhakimutomo@gmail.com>
2022-07-02 00:09:10 +0700

    crafting material features

--- commands/apex.js
+++ commands/apex.js
@@ -4,26 +4,26 @@ const { default: axios } = require("axios")
 module.exports = {
     name : 'apex',
     async execute(message, args) {
-        if (!args.length) {
+		if (!args.length) {
 			return message.channel.send(`You have not input any arguments, ${message.author} :(`)
 		}
 		else if (args[0] === 'map') {
+			const mapRotationEmbed = {
+				color: 0x0099ff,
+				title: 'Lorem Ipsum',
+				fields: [
+					{
+						name: 'Current',
+						value: 'lorem ipsum',
+					},
+					{
+						name: 'Next',
+						value: 'lorem ipsum',
+					},
+				],
+				timestamp: new Date(),
+			};
 			try {
-				const mapRotationEmbed = {
-					color: 0x0099ff,
-					title: 'Lorem Ipsum',
-					fields: [
-						{
-							name: 'Current',
-							value: 'lorem ipsum',
-						},
-						{
-							name: 'Next',
-							value: 'lorem ipsum',
-						},
-					],
-					timestamp: new Date(),
-				};
 				const getMap = async() => {
 					const mapRotation = await axios.get(`https://api.mozambiquehe.re/maprotation?auth=${process.env.APEX_API_KEY}&version=2`)
 					const data = mapRotation.data
@@ -37,7 +37,7 @@ module.exports = {
 						mapRotationEmbed.fields[0].value = `${data.ranked.current.map}\nTime left : ${data.ranked.current.remainingTimer}`
 						mapRotationEmbed.fields[1].value = `${data.ranked.next.map}`
 					}
-					else {
+					else if (args[1] === undefined) {
 						mapRotationEmbed.title = "Battle Royale Map Rotation"
 						mapRotationEmbed.fields[0].value = `${data.battle_royale.current.map}\nTime left : ${data.battle_royale.current.remainingTimer}`
 						mapRotationEmbed.fields[1].value = `${data.battle_royale.next.map}`
@@ -52,7 +52,24 @@ module.exports = {
 		}
 
         else if (args[0] === 'crafting') {
+			try {
+				const getCrafting = async() => {
+					const craftingRotation = await axios.get(`https://api.mozambiquehe.re/crafting?auth=${process.env.APEX_API_KEY}`)
+					const data = craftingRotation.data;
+					await message.channel.send(
+						data[0].bundleContent
+					)
 
+				}
+				getCrafting()
+			}
+			catch(e) {
+				await message.channel.send("Error, something is wrong :(")
+			}
         }
+
+		else {
+			await message.channel.send("I don't understand what it means :(")
+		}
     }
 }
