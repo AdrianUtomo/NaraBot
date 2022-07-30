1a67d19ef5948b047eb5ab617a5d7d4445df576e
Adrian <adrianhakimutomo@gmail.com>
2022-07-30 19:19:52 +0700

    added global slash commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -14,8 +14,24 @@ for (const file of commandFiles) {
 	slashCommands.push(command.data.toJSON())
 }
 
-const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
-rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: slashCommands })
-	.then(() => console.log('Successfully registered application commands.'))
-	.catch(console.error);
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+			{ body: slashCommands },
+		);
+
+		await rest.put(
+			Routes.applicationCommands(process.env.CLIENT_ID),
+			{ body: slashCommands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
